969b5af02e8cfa6f949a36ce2e2f9fcb2cf5dacf
Emmalyn Ramos <elr9@njit.edu>
2017-11-07 12:04:52 -0500

    Adding check email function

--- actions.php
+++ actions.php
@@ -1,4 +1,5 @@
 <?php require "db.php" ?>
+
 <?php
 
 $fname = $_POST['fname'];
@@ -7,23 +8,32 @@ $email = $_POST['email'];
 $pnumber = $_POST['pnumber'];
 $birthday = $_POST['birthday'];
 
-
 echo "First Name: " . $fname . "<br>"; 
 echo "Last Name: " . $lname . "<br>"; 
 echo "Email: " . $email . "<br>";
 echo "Phone Number: " . $pnumber . "<br>";
-echo "Birthday: " . $birthday. "<br>"; 
+echo "Birthday: " . $birthday. "<br><br>"; 
 
 
-// $query = $con->prepare( "SELECT `email` FROM accounts WHERE email = 'doratheexplorer@nick.com'" );
-// $query->bindValue( 1, $email );
-// $query->execute();
+function checkEmail($email){
+	$sql = "SELECT email FROM accounts WHERE email = '$email'";
+	$results = runQuery($sql);
 
-// if( $query->rowCount() > 0 ) { # If rows are found for query
-//      echo "Email found!";
-// }
-// else {
-//      echo "Email not found!";
-// }
+	if( count($results) > 0 ) { # If rows are found for query
+     echo "Email found! " . $email;
+	}
+	else {
+	     echo "Email not found! " . $email;
+	}
+
+}
+	
+checkEmail($email);
+
+//checkEmail(); 
+
+
+
+
 
 ?>
