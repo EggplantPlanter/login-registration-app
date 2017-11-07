61340a5aa00da7ff39dfaee13959d4efa9c2cc6d
Emmalyn Ramos <elr9@njit.edu>
2017-11-07 00:32:38 -0500

    Added code into db.php file

---
+++ db.php
@@ -0,0 +1,44 @@
+<?php
+
+//---------------- [CONNECTING TO DATABASE] ----------------
+$hostname = "sql.njit.edu";
+$username = "elr9";
+$password = "barnes26";
+$conn = elr9;
+
+try{
+
+	$conn = new PDO("mysql:host=$hostname;dbname=elr9",
+	$username, $password);
+	echo "SUCCESSFULLY connected to database $username <br><br>"; 
+
+}//END TRY BLOCK
+
+catch(PDOException $e){
+    	// echo "Connection failed: " . $e->getMessage();
+		http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage());
+
+}//END CATCH BLOCK
+
+// Runs SQL query and returns results (if valid)
+function runQuery($query){
+	global $conn;
+    try {
+		$q = $conn->prepare($query);
+		$q->execute();
+		$results = $q->fetchAll();
+		$q->closeCursor();
+		return $results;	
+	} catch (PDOException $e) {
+		http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage());
+	}	  
+}
+
+function http_error($message){
+	header("Content-type: text/plain");
+	die($message);
+}
+
+
+
+?>
