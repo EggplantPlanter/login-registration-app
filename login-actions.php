e1e5ebed65721bbb4522e815a2cecd7391ef9c44
Emmalyn Ramos <elr9@njit.edu>
2017-11-08 20:54:46 -0500

    aded redirect to login page

--- login-actions.php
+++ login-actions.php
@@ -1,1 +1,46 @@
 <?php require "db.php" ?>
+
+<?php
+
+$email = $_POST['email'];
+$password = $_POST['pass'];
+
+echo "Email: " . $email . "<br>";
+echo "Password: " . $password. "<br><br>"; 
+
+if(isset($email, $password)){
+	$sql = "SELECT * FROM accounts WHERE email = '$email' AND password='$password'";
+	$results = runQuery($sql);
+
+	if( count($results) > 0 ) {
+		foreach ($results as $row){
+			$firstname = $row['fname'];
+			$lastname = $row['lname'];
+		}
+
+    	echo "Hello " . $firstname . " " . $lastname;
+    	
+	}
+
+	else {
+	    echo "Go away. That email does not exist <br>";
+	    echo "Redirecting you to previous page";
+	    header("refresh:3; url=index.php");
+	    //Tell user to go to previous page
+
+	}
+}
+	
+
+	
+
+	//grab variables of user from sql 
+	//assign them to $names
+
+/*
+TO DO:
+- Tell user to go to previous page
+- insert home button
+*/
+
+
